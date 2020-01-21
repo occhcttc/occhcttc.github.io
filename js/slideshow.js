94e77a8640fc59eed9770096b97d269202741aa1
edweenie123 <edwinchen1000@gmail.com>
2020-01-21 08:57:33 -0500

    added sketchy comments and changed links and resources section

--- js/slideshow.js
+++ js/slideshow.js
@@ -4,6 +4,14 @@ window.onresize = setSlideShowHeight
 var slides = document.getElementsByClassName("slide");
 var slideIndex = 1;
 
+
+
+
+
+
+
+
+
 showSlides(slideIndex);
 automaticSlides(1);
 
@@ -17,21 +25,37 @@ function setSlideShowHeight() {
 }
 
 function plusSlides(n) {
+    // shows slide slideIndex + n
     showSlides(slideIndex += n);
 }
 
-function showSlides(n) {
-    var i;
 
+
+
+
+
+
+
+
+
+
+
+function showSlides(n) {
+    // cycles back to slide 1 if n is too large
     if (n > slides.length) 
         slideIndex = 1;
+
+    // cycles back to the last slide if n is too small
     if (n < 1) 
-        slideIndex = slides.length ;
-    for (i = 0; i < slides.length; i++) 
+        slideIndex = slides.length; // slides.length returns the # of slides
+    
+    // Iterates through all the slides and makes them invisible
+    for (var i = 0; i < slides.length; i++) 
         slides[i].style.display = "none";
 
+    // set slide n to be the only slide visible
+    // we take slides at index slideIndex - 1 because JavaScript used 0-based indexing 
     slides[slideIndex - 1].style.display = "block";
-    
 }
 
 function automaticSlides(n) {
